4a54c96df37c0d32d9c31eea6c27dde067326d8a
Andrea <andreaflongo@gmail.com>
2023-06-06 11:38:22 +0200

    fix hunting

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from drf_spectacular.views import (
-#     SpectacularAPIView,
-#     SpectacularSwaggerView
-# )
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
 
 from django.contrib import admin
 from django.urls import path, include
@@ -26,8 +26,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),name='api-docs',),
     path('api/user/', include('user.urls')),
-    # path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    # path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),name='api-docs',
-    # ),
 ]
